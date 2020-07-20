38e7c83a9de8a132463bee4b4a37e3560add6aa9
Inderjit Singh <erinderbrar1@gmail.com>
2020-07-20 11:36:08 -0400

    Initial commit

---
+++ app/src/main/java/com/him/note_him_android/MainActivity.java
@@ -0,0 +1,113 @@
+package com.him.note_him_android;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.him.note_him_android.adapter.SubjectAdapter;
+import com.him.note_him_android.database.DatabaseHelper;
+import com.him.note_him_android.database.Subject;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
+    FloatingActionButton addSubject;
+    RecyclerView subjectRecycler;
+    SubjectAdapter subjectAdapter;
+    boolean updateSubject = false;
+    int id=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addSubject = findViewById(R.id.addSubject);
+        subjectRecycler = findViewById(R.id.subjectRecycler);
+        databaseHelper = new DatabaseHelper(this);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            id = bundle.getInt("id");
+            updateSubject = true;
+        }
+        addSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNoteDialog(false, null, 0);
+            }
+        });
+
+        subjectAdapter = new SubjectAdapter(databaseHelper.getAllSubject(), this, updateSubject,id,databaseHelper);
+        subjectRecycler.setAdapter(subjectAdapter);
+    }
+
+    /**
+     * Shows alert dialog with EditText options to enter / edit
+     * a note.
+     * when shouldUpdate=true, it automatically displays old note and changes the
+     * button text to UPDATE
+     */
+    private void showNoteDialog(final boolean shouldUpdate, final Subject note, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.subject_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputNote = view.findViewById(R.id.note);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(!shouldUpdate ? getString(R.string.lbl_new_note_title) : getString(R.string.lbl_edit_note_title));
+
+        if (shouldUpdate && note != null) {
+            inputNote.setText(note.getSubject());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "update" : "save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+
+                    }
+                })
+                .setNegativeButton("cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show toast message when no text is entered
+                if (TextUtils.isEmpty(inputNote.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Enter Subject!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+                // check if user updating note
+                if (shouldUpdate && note != null) {
+                    // update note by it's id
+//                    updateNote(inputNote.getText().toString(), position);
+                } else {
+                    // create new note
+//                    createNote(inputNote.getText().toString());
+                    databaseHelper.insertSubject(inputNote.getText().toString());
+                }
+                subjectAdapter.update(databaseHelper.getAllSubject());
+            }
+        });
+    }
+
