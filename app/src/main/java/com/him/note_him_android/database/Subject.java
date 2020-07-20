1ff575563c61a55d982108079b2626121b1daba6
Inderjit Singh <erinderbrar1@gmail.com>
2020-07-20 11:37:55 -0400

    Subject

---
+++ app/src/main/java/com/him/note_him_android/database/Subject.java
@@ -0,0 +1,25 @@
+package com.him.note_him_android.database;
+
+public class Subject {
+    public static final String TABLE_NAME = "subject";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_SUBJECT = "subejct";
+    String subject;
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
